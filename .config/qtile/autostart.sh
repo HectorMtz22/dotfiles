f2b3430687741ea164482b9f52504d8efb6c21ef
HectorMtz22 <hec.gre81@gmail.com>
2022-04-17 14:56:20 -0500

    Deleted outdated configurations

--- .config/qtile/autostart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# systray battery icon
-#cbatticon -u 5 &
-# systray volume
-#volumeicon &
-#feh --bg-scale ~/Pictures/wallpaper.jpg &
-
--- install_old.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash 
-
-cp -R -f ./.config/ $HOME
-cp -R -f ./.oh-my-zsh/ $HOME
-cp -f ./.vimrc $HOME/.vimrc
-cp -f ./.zshrc $HOME/.zshrc
-# cp ./.p10k.zsh $HOME/.p10k.zsh
-
